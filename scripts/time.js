3631bfad7d06f74405095e64ff0cbb2cee5faef3
hirschet <ethan@hirschfamily.com>
2019-04-07 20:09:55 -0700

    more minor changes

--- scripts/time.js
+++ scripts/time.js
@@ -2,11 +2,27 @@ function setTime(){
 
   var today = new Date();
 
-  document.getElementById("time").innerHTML = ((today.getHours() < 10)?"0":"") + today.getHours() + ":" + ((today.getMinutes() < 10)?"0":"") + today.getMinutes();
+  document.getElementById("hours").innerHTML = ((today.getHours() < 10)?"0":"") + today.getHours();
+  document.getElementById("mins").innerHTML = ((today.getMinutes() < 10)?"0":"") + today.getMinutes();
   document.getElementById("date").innerHTML = today.toDateString();
 
 }
 
+// function setColon(colon){
+//   // if (colon.set == true){
+//   //   document.getElementById("time").innerHTML = ":";
+//   //   colon.set = false;
+//   // }
+//   // else{
+//   //   document.getElementById("time").innerHTML = " ";
+//   //   colon.set = true;
+//   // }
+//   // document.getElementById("time").innerHTML = ":";
+// }
+
+// var colon = colonFlag;
+
 setTime();
 
 setInterval(setTime, 1000);
+// setInterval(setColon(colon), 500);
